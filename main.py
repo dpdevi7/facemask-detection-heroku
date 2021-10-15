b34846f0b7c1d8e7b5be8aa6f0673660f6994839
Devi Prasad <dp.devi03@gmail.com>
2021-10-16 00:36:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from detectionUtils import getAllowedFileExtension
 import detectionUtils
 import os
@@ -7,38 +7,48 @@ import os
 app = Flask(__name__)
 app.config["DEBUG"] = False # for deployment make it False
 
+# 1 load model, always load model in global variable, so that every time a request hits, you dont need to
+#   load model again and again.
+MODEL = detectionUtils.loadModelPretrained()
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
 
     responseObject = None
+    statusCode = None
+    print('filenames' in request.files)
 
     if request.method == 'POST':
-        imageFile = request.files['filename']
-        imageFileName = imageFile.filename
+        if 'filename' in request.files:
+            imageFile = request.files['filename']
+            imageFileName = imageFile.filename
 
-        if getAllowedFileExtension(imageFileName):
-            imgBytes = imageFile.read()
-            imgTensor = detectionUtils.readImage(imgBytes)
-            # 1 load model
+            if getAllowedFileExtension(imageFileName):
+                imgBytes = imageFile.read()
+                imgTensor = detectionUtils.readImageNonScriptedModule(imgBytes)
+                # 2 prediction
+                boxes, labels, scores = detectionUtils.detectMaskNonScriptedModule(MODEL, imgTensor)
+                # 3 if predictions are not empty
+                # 4 return result
+                responseObject = {
+                    'success':True,
+                    'result':{
+                        'boxes': boxes,
+                        'labels': labels,
+                        'scores': scores
+                    }
+                }
 
-            MODEL = detectionUtils.get_ssd_from_checkpoint(modelFile=os.path.join('model-dir','ssd.pth'),
-                                                           SSD_MODEL=detectionUtils.get_ssd_model(4, 512))
-            # 2 prediction
-            boxes, labels, scores = detectionUtils.detectMask(MODEL, imgTensor)
-            # 3 return result
+                statusCode = 200
+        else:
             responseObject = {
-                'boxes':boxes,
-                'labels':labels,
-                'scores':scores
+                'msg':"please send file with key -- filename",
+                'success':False
             }
-
-
-
-
-    return jsonify(responseObject)
+            statusCode = 400
+    return jsonify(responseObject), statusCode
 
 # if __name__ == "__main__":
-#     # app.run(host='127.0.0.1', port=5000)
-#     app.run(host='0.0.0.0', port=5000)
+#     app.run(host='127.0.0.1', port=8080)
 # https://www.youtube.com/watch?v=bA7-DEtYCNM&t=528s
